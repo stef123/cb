577a2d8d44ba26bfa141d3b6c78fe2bafd2d0e4b
Stefan Nilsson <stefan@jest.se>
2012-02-29 01:01:44 +0100

    f

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.get('/', function(req, res) {
         	
         	
            var result = resultSet[i];
-           response.write('<h1>' + result.name + '</h1><br/>');
+           res.write('<h1>' + result.name + '</h1><br/>');
 
         }    
 
