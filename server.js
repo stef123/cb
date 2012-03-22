3022ce9363243b374f0864d3fbf88bb44ce769b1
Stefan Nilsson <stefan@jest.se>
2012-03-22 12:10:31 +0100

    v

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ var fs = require('fs');
 var sys = require('sys');
 var express = require('express');
 var expose = require('express-expose');
-var io = require('socket.io');
-
-io.listen(3000);
+var io = require('socket.io').listen(3000);
 var formattedDatetime;
 var resultSet;
 
