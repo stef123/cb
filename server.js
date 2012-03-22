c1136ce34c7fb22fc62c50639c1cf055ec5cc6af
Stefan Nilsson <stefan@jest.se>
2012-03-22 12:22:10 +0100

    rer

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var fs = require('fs');
 var sys = require('sys');
 var express = require('express');
 var expose = require('express-expose');
-var io = require('socket.io');
+var io = require('socket.io').listen(3000);
+
+console.log(io);
 var formattedDatetime;
 var resultSet;
 
